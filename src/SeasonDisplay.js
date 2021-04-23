620901e80f9031e67dedc047cebcfeb832410d49
Maduri Vassaramo <maduri@vinterior.co>
2021-04-23 10:13:14 +0200

    add logic to SeasonDisplay to show correct season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "Summer" : "Winter"
+  } else {
+    return lat < 0 ? "Winter" : "Summer"
+  }
+}
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth())
+  
   return(
-    <div>Season Display!</div>
+    <div>
+      <h1>Season Display!</h1>
+      <p>{props.lat}</p>
+    </div>
   )
 }
 
