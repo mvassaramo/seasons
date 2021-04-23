982a2a759dc73d7d23ef0b6325e100f502ec6b3e
Maduri Vassaramo <maduri@vinterior.co>
2021-04-23 17:03:07 +0200

    refactor render logic into its own method

--- src/index.js
+++ src/index.js
@@ -16,15 +16,22 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <Error message={this.state.errorMessage}/>
     }
-
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat}/>
     }
     return <Spinner message="Please accept location request"/>
+  }
+
+  render() {
+    return(
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    )
   }
 }
 
