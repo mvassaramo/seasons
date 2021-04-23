92f875eb1ac31169e0ffc51ff386dd4bb16bbb2f
Maduri Vassaramo <maduri@vinterior.co>
2021-04-23 16:03:26 +0200

    refactored season ternary operator

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,20 +1,35 @@
 import React from 'react';
 
+
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: 'sun'
+  },
+  winter: {
+    text: "Burr, it's chilly!",
+    iconName: 'snowflake'
+  }
+}
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
-    return lat > 0 ? "Summer" : "Winter"
+    return lat > 0 ? "summer" : "winter"
   } else {
-    return lat < 0 ? "Winter" : "Summer"
+    return lat < 0 ? "winter" : "summer"
   }
 }
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth())
-  
+  const { text, iconName } = seasonConfig[season]
+
+
   return(
     <div>
-      <h1>Season Display!</h1>
-      <p>{props.lat}</p>
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
     </div>
   )
 }
