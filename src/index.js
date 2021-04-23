3c1984c072aa7713587c7852734c5fbb1a0f715e
Maduri Vassaramo <maduri@vinterior.co>
2021-04-23 09:35:14 +0200

    move gelocation request to componentDidMount method

--- src/index.js
+++ src/index.js
@@ -3,24 +3,36 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super(props);
-
+    // This is the ONLY time we do direct assignment
+    // to this.state
     this.state = {
-      lat: null
+      lat: null,
+      errorMessage: ''
     };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude })
       },
-      (err) => console.log(err) 
+      (err) => console.log(this.setState({ errorMessage: err.message })) 
     );
   }
 
+  componentDidUpdate(){
+    console.log('UPDATE')
+  }
 
   render() {
-    return(
-      <div>Latitude: {this.state.lat}</div>
-    )
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+    return <div>Loading!</div>
   }
 }
 
