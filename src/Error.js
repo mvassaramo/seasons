2c0161de750942d1dabe5d64c416a29f95fa1efc
Maduri Vassaramo <maduri@vinterior.co>
2021-04-23 17:40:32 +0200

    pass props down to Error component

--- src/Error.js
+++ src/Error.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 
-const Error = () => {
+const Error = (props) => {
   return(
-    <div>Oops! something is wrong!</div>
+    <div>{props.message}</div>
   )
+}
+
+Error.defaultProps = {
+  message: "Oops, something went wrong!"
 }
 
 export default Error;
